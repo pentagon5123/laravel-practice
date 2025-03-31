d9f1c6f9fd86b6fcafec566941055fae80f99938
Tsuji Haruto <haruto05012003@i.softbank.jp>
2025-03-31 11:26:31 +0000

    create post menu

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+</head>
+
+<body>
+    <h1>{{$post->title}}</h1>
+    <div>
+
+        <h3>
+            本文
+        </h3>
+        <p> {{$post -> body}}</p>
+
+    </div>
+    <div>
+        <a href="/">戻る</a>
+    </div>
+</body>
